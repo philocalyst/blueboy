a933794bc985730e0001b7f8c1d2d7b5882b8bbb
Miles Wirht <regime_skylark.0y@icloud.com>
2025-03-17 21:19:07 -0400

    Initial commit of objc class
    Defines a class inherting nsobject that binds to the private framework I
    am using to manage a disconnect event. Looks for a notification to
    complete the removal

--- Sources/bluetooth/bluetooth.swift
+++ Sources/bluetooth/bluetooth.swift
@@ -1,3 +1,20 @@
 import ArgumentParser
 import Foundation
 import IOBluetooth
+
+class DeviceNotificationRunLoopStopper: NSObject {
+    private var expectedDevice: IOBluetoothDevice?
+
+    init(withExpectedDevice device: IOBluetoothDevice) {
+        expectedDevice = device
+    }
+
+    @objc func notification(
+        _ notification: IOBluetoothUserNotification, fromDevice device: IOBluetoothDevice
+    ) {
+        if expectedDevice == device {
+            notification.unregister()
+            CFRunLoopStop(CFRunLoopGetCurrent())
+        }
+    }
+}
