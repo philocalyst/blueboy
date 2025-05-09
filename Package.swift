8db12e609be0b1d4bde65373b63c396d47e627ae
unknown <regime_skylark.0y@icloud.com>
2025-05-08 22:53:14 -0400

    💫CHANGED💫 Name to BlueBoy
    Not that much of a ripoff..

--- Package.swift
+++ Package.swift
@@ -2,34 +2,34 @@
 import PackageDescription
 
 let package = Package(
-    name: "Blue",
-    platforms: [
-        .macOS(.v10_15)
-    ],
-    products: [
-        .library(
-            name: "BlueKit",
-            targets: ["BlueKit"]),
-        .executable(
-            name: "blueutil",
-            targets: ["Blue"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.0"),
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.4.0"),
-    ],
-    targets: [
-        .target(
-            name: "BlueKit",
-            dependencies: [
-                .product(name: "Logging", package: "swift-log")
-            ]),
-        .executableTarget(
-            name: "Blue",
-            dependencies: [
-                "BlueKit",
-                .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                .product(name: "Logging", package: "swift-log"),
-            ]),
-    ]
+  name: "Blue",
+  platforms: [
+    .macOS(.v10_15)
+  ],
+  products: [
+    .library(
+      name: "BlueKit",
+      targets: ["BlueKit"]),
+    .executable(
+      name: "BlueBoy",
+      targets: ["BlueBoy"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.0"),
+    .package(url: "https://github.com/apple/swift-log.git", from: "1.4.0"),
+  ],
+  targets: [
+    .target(
+      name: "BlueKit",
+      dependencies: [
+        .product(name: "Logging", package: "swift-log")
+      ]),
+    .executableTarget(
+      name: "BlueBoy",
+      dependencies: [
+        "BlueKit",
+        .product(name: "ArgumentParser", package: "swift-argument-parser"),
+        .product(name: "Logging", package: "swift-log"),
+      ]),
+  ]
 )
