237591f78f95a21d08b4dbfb2564b17ba0bc0f90
unknown <regime_skylark.0y@icloud.com>
2025-05-08 22:58:12 -0400

    💫CHANGED💫 Executable name to bboy
    For conciseness

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
       name: "BlueKit",
       targets: ["BlueKit"]),
     .executable(
-      name: "BlueBoy",
+      name: "bboy",
       targets: ["BlueBoy"]),
   ],
   dependencies: [
