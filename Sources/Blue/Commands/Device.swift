2050868a325de9377fbaaf342b078919a5492d84
unknown <regime_skylark.0y@icloud.com>
2025-05-08 22:41:54 -0400

    💫CHANGED💫 Device subcommand now has subcommands of its own!
    Think this makes it a lot easier to use honestly... I associate options with a mulitplicity, but that's never been true for this project.

--- Sources/Blue/Commands/Device.swift
+++ Sources/Blue/Commands/Device.swift
@@ -3,7 +3,6 @@ import BlueKit
 import Foundation
 import Logging
 
-/// ▰▰▰ Device command for interacting with specific devices ▰▰▰
 struct DeviceCommand: ParsableCommand {
   static let configuration = CommandConfiguration(
     commandName: "device",
@@ -13,84 +12,74 @@ struct DeviceCommand: ParsableCommand {
   @Argument(help: "Device ID (address or name)")
   var id: String
 
-  @Flag(name: .customLong("info"), help: "Show information about device")
-  var info: Bool = false
+  @Argument(help: "An action to perform")
+  var action: Action
 
-  @Flag(name: .customLong("is-connected"), help: "Connected state of device as 1 or 0")
-  var isConnected: Bool = false
-
-  @Flag(name: .customLong("connect"), help: "Create a connection to device")
-  var connect: Bool = false
-
-  @Flag(name: .customLong("disconnect"), help: "Close the connection to device")
-  var disconnect: Bool = false
-
-  @Flag(name: .customLong("pair"), help: "Pair with device, optional PIN")
-  var pair: Bool = false
-
-  @Option(name: .customLong("pin"), help: "PIN for pairing (optional)")
+  @Option(
+    name: .long,
+    help: "PIN for pairing (only used with `pair` action)"
+  )
   var pin: String?
 
-  @Flag(name: .customLong("unpair"), help: "EXPERIMENTAL unpair the device")
-  var unpair: Bool = false
+  enum Action: String, CaseIterable, ExpressibleByArgument {
+    case info
+    case isConnected = "is-connected"
+    case connect
+    case disconnect
+    case pair
+    case unpair
+  }
 
   func run() throws {
     let logger = BlueUtilLogger.logger
-    let deviceManager = DeviceManager(logger: logger)
+    let mgr = DeviceManager(logger: logger)
 
-    logger.info("Running Device command for ID: \(id)")
-
-    if info {
-      try showDeviceInfo(id, deviceManager: deviceManager)
-    } else if isConnected {
-      try checkDeviceConnection(id, deviceManager: deviceManager)
-    } else if connect {
-      try connectToDevice(id, deviceManager: deviceManager)
-    } else if disconnect {
-      try disconnectFromDevice(id, deviceManager: deviceManager)
-    } else if pair {
-      try pairWithDevice(id, pin: pin, deviceManager: deviceManager)
-    } else if unpair {
-      try unpairDevice(id, deviceManager: deviceManager)
-    } else {
-      logger.info("No specific device action selected for \(id)")
-      print("No specific device action specified for ID \(id)")
+    switch action {
+    case .info:
+      try showInfo(mgr)
+    case .isConnected:
+      try checkConnected(mgr)
+    case .connect:
+      try doConnect(mgr)
+    case .disconnect:
+      try doDisconnect(mgr)
+    case .pair:
+      try doPair(mgr)
+    case .unpair:
+      try doUnpair(mgr)
     }
   }
 
-  private func showDeviceInfo(_ id: String, deviceManager: DeviceManager) throws {
-    let logger = BlueUtilLogger.logger
-    logger.info("Showing info for device \(id)")
-    let device = try deviceManager.getDevice(identifier: id)
+  private func showInfo(_ mgr: DeviceManager) throws {
+    BlueUtilLogger.logger.info("Showing info for \(id)")
+    let device = try mgr.getDevice(identifier: id)
     print("Device Information:")
-    let deviceInfo = deviceManager.listDevices([device], options: DevicePrintOptions.all)
-    for line in deviceInfo {
-      print(line)
-    }
+    listDevices([device], options: .all)
+      .forEach { print($0) }
   }
 
-  private func checkDeviceConnection(_ id: String, deviceManager: DeviceManager) throws {
-    let isConnected = try deviceManager.isDeviceConnected(id)
-    print(isConnected ? "1" : "0")
+  private func checkConnected(_ mgr: DeviceManager) throws {
+    let connected = try mgr.isDeviceConnected(id)
+    print(connected ? "1" : "0")
   }
 
-  private func connectToDevice(_ id: String, deviceManager: DeviceManager) throws {
-    try deviceManager.connectToDevice(id)
+  private func doConnect(_ mgr: DeviceManager) throws {
+    try mgr.connectToDevice(id)
     print("Successfully connected to device \(id)")
   }
 
-  private func disconnectFromDevice(_ id: String, deviceManager: DeviceManager) throws {
-    try deviceManager.disconnectFromDevice(id)
+  private func doDisconnect(_ mgr: DeviceManager) throws {
+    try mgr.disconnectFromDevice(id)
     print("Successfully disconnected from device \(id)")
   }
 
-  private func pairWithDevice(_ id: String, pin: String?, deviceManager: DeviceManager) throws {
-    try deviceManager.pairWithDevice(id, pin: pin)
+  private func doPair(_ mgr: DeviceManager) throws {
+    try mgr.pairWithDevice(id, pin: pin)
     print("Successfully paired with device \(id)")
   }
 
-  private func unpairDevice(_ id: String, deviceManager: DeviceManager) throws {
-    try deviceManager.unpairDevice(id)
+  private func doUnpair(_ mgr: DeviceManager) throws {
+    try mgr.unpairDevice(id)
     print("Successfully unpaired device \(id)")
   }
 }
