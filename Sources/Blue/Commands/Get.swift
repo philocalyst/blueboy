7a370fc04cfc19ff80a8ff38c6318138c0b20bbc
unknown <regime_skylark.0y@icloud.com>
2025-05-08 22:43:03 -0400

    💫CHANGED💫 Now using subcommand structure rather than options

--- Sources/Blue/Commands/Get.swift
+++ Sources/Blue/Commands/Get.swift
@@ -11,12 +11,6 @@ struct GetCommand: ParsableCommand {
   )
 
   @Flag(
-    name: [.short, .customLong("discoverable")],
-    help: "Output discoverable state as 1 or 0"
-  )
-  var discoverableStateOutput: Bool = false
-
-  @Flag(
     name: [.short, .customLong("power")],
     help: "Output power status as 1 or 0"
   )
@@ -28,23 +22,14 @@ struct GetCommand: ParsableCommand {
 
     logger.info("Running Get command")
 
-    if discoverableStateOutput {
-      try getDiscoverableState(bluetoothManager: bluetoothManager)
-    }
-
     if powerStateOutput {
       try getPowerState(bluetoothManager: bluetoothManager)
     }
 
-    if !discoverableStateOutput && !powerStateOutput {
+    if !powerStateOutput {
       logger.warning("No state retrieval specified")
       print("No state retrieval specified")
     }
-  }
-
-  private func getDiscoverableState(bluetoothManager: BluetoothManager) throws {
-    let isDiscoverable = try bluetoothManager.getDiscoverableState()
-    print(isDiscoverable ? "1" : "0")
   }
 
   private func getPowerState(bluetoothManager: BluetoothManager) throws {
